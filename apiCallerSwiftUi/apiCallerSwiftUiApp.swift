cb85084262e173fca24159e7fc843017122db7e7
Aarish Khanna <aarishkhanna@gmail.com>
2023-01-11 12:07:35 +0530

    Initial Commit

---
+++ apiCallerSwiftUi/apiCallerSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiCallerSwiftUiApp.swift
+//  apiCallerSwiftUi
+//
+//  Created by Aarish Khanna on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct apiCallerSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
